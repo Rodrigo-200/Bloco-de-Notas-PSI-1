c1560aed5d484b0a98ca926aeb5051706745c58d
Rodrigo Martins <I220099@inete.net>
2024-11-13 15:49:34 +0000

    Mais apontamentos

--- Bloco de Notas PSI 1/Form1.Designer.cs
+++ Bloco de Notas PSI 1/Form1.Designer.cs
@@ -29,21 +29,67 @@ namespace Bloco_de_Notas_PSI_1
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.txt_Path = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(78, 55);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(575, 284);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // txt_Path
+            // 
+            this.txt_Path.Location = new System.Drawing.Point(123, 27);
+            this.txt_Path.Name = "txt_Path";
+            this.txt_Path.Size = new System.Drawing.Size(100, 22);
+            this.txt_Path.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(455, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_Path);
+            this.Controls.Add(this.listView1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.TextBox txt_Path;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Bloco de Notas PSI 1/Form1.cs
+++ Bloco de Notas PSI 1/Form1.cs
@@ -75,10 +75,154 @@ namespace Bloco_de_Notas_PSI_1
 
             /*
              * DirectoryInfo retorna toda a infrmação de um diretorio
+             * FullName comporta-se como um full path do ficheiro/diretorio ja o name é so o nome do ficheiro/diretorio
              */
 
-            
+            txt_Path.Text = path; //Mostra o path
+            DirectoryInfo dir = new DirectoryInfo(path);
+            foreach (var item in dir.GetDirectories()) // Vai a cada diretorio dentro do diretorio "path"
+            {
+                //podemos adicionar a um controlo como uma listview
+                //Exempo
+                listView1.Items.Add(new ListViewItem(item.Name, 0));
+            }
+            foreach (var item in dir.GetFiles()) // Vai a cada diretorio dentro do diretorio "path" e vai buscar todos os ficheiros
+            {
+                //podemos adicionar a um controlo como uma listview
+                listView1.Items.Add(new ListViewItem(item.Name, 1));
+            }
 
+            /*
+             * Podemos nao ter acesso a alum diretorio logo a importancia dos try... catch
+             */
+
+            path = path += listView1.SelectedItems[0].Text + @"\";
+
+            //back
+
+            path = new DirectoryInfo(path).Parent.FullName;
+
+            dir.Attributes = FileAttributes.Hidden; //hidden files.
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamWriter_StreamReader_BinaryWriter_BinaryReader();
+        }
+
+
+
+
+        private void StreamWriter_StreamReader_BinaryWriter_BinaryReader()
+        {
+            //Leitura e escrita de ficheiros 
+
+            //Criação e escrita de ficheiros com bytes (apenas em bytes!!!)
+
+            //Instenciar um objeto do tipo stream e o unico campo obrigatório é o caminho (absoluto)
+            FileStream f = new FileStream("Numbers.txt", FileMode.Create, FileAccess.Write, FileShare.None); 
+
+            //Para escrever coisas é preciso converter para bytes e "meter lá para dentro"
+            byte[] tesss = new UTF8Encoding().GetBytes("Teste escrever em ficheiro");
+
+            //bytes, inicio, lenght...
+            f.Write(tesss, 3, tesss.Length - 3);
+
+            for(int i = 0; i < 10; i++)
+            {
+                byte[] number = new UTF8Encoding().GetBytes(i.ToString());
+                f.Write(number, 0, number.Length);
+            }
+            f.Close(); //Se não fecharmos depois dá erro a dizer que está em utilização
+
+
+            FileStream fbinary = new FileStream("Numbers.txt", FileMode.Create, FileAccess.Write, FileShare.None);
+
+            BinaryWriter bw = new BinaryWriter(fbinary, Encoding.UTF8);
+            bw.Write("INETE");
+            bw.Write(true);
+            bw.Write(1);
+            bw.Write(12);
+            bw.Write(14);
+            bw.Write(12);
+            bw.Close();
+
+
+            FileStream fbinaryread = new FileStream("Numbers.txt", FileMode.Open);
+            BinaryReader br = new BinaryReader(fbinaryread, Encoding.ASCII);
+            string str = br.ReadString();
+            bool bol = br.ReadBoolean();
+            int k = br.ReadInt32();
+            int a = br.ReadInt32();
+            int b = br.ReadInt32();
+            int c = br.ReadInt32();
+            int rr = br.ReadInt32(); //O programa rebenta porque vai ler "nada (null)" metemos aqui propositalmente para ver o erro
+            br.Close();
+
+            // --------------------------------- //
+
+            //StreamWriter trabalha com texto
+            //Write não cria uma nova linha writelinha cria uma nova linha
+            StreamWriter sw2 = new StreamWriter("comment.txt");
+            sw2.WriteLine("INETE" + Environment.NewLine + "Escola");
+            sw2.Write(true);
+            sw2.Write(1);
+            sw2.Close();
+
+            //StreamReader 
+            StreamReader sr = new StreamReader("comment.txt");
+            //Ler linha a linha
+            while (!sr.EndOfStream)
+                MessageBox.Show(sr.ReadLine());
+            sr.Close();
+
+
+
+            sr = new StreamReader("Numbers.txt");
+            //Ler caracter a caracter
+            while (!sr.EndOfStream)
+                MessageBox.Show(((char)sr.Read()).ToString());
+            sr.Close();
+
+
+            sr = new StreamReader("comment.txt");
+            //Ler Tudo
+                MessageBox.Show(sr.ReadToEnd());
+            sr.Close();
+
+
+            //Trabalhar com strings em vez de ficheiros
+
+            var text = @"Olá
+                         olé
+                         fixe";
+
+
+
+            //Com isto podemos ler linha a linha com as quebras de texto já que se fosse só uma string não teria as quebras de linha
+            StringReader srd = new StringReader(text);
+            string line;
+
+            while((line = srd.ReadLine()) != null)
+            {
+                MessageBox.Show(line);
+            }
+        }
+
+        private void XML()
+        {
+            //Ver alunos.xml serve para transmitir informações
+
+            //Pesquisar Grid Systems
+
+            //Exemplos BootStrap
+
+            // Web Service
+
+            //Usa-se para comunicar com uma base de dados de maneira mais segura, serve de intermediario entre as conexões/comunicações
+
+            // Um site responsive adapta-se as dimensões do ecrã do dispositivo
 
         }
     }
